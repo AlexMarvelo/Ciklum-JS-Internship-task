0956bd0957654fed39d8b6147e2fc76b66250a7a
AlexMarvelo <mironenkoalexey@gmail.com>
2016-07-13 22:49:39 +0300

    featured block completed desktop footer completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,11 @@
 class App {
     constructor(){
         this.pageHeader = require('../blocks/pageHeader/pageHeader.js');
-        this.pageFooter = require('../blocks/pageFooter/pageFooter.js');
         this.subscribe = require('../blocks/subscribe/subscribe.js');
     }
 
     init(){
         this.pageHeader.init();
-        this.pageFooter.init();
         this.subscribe.init();
     }
 }
